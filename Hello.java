d6aa248d4823897edb8231f3a6a8389bd719d419
Itay Finkelshtein <itayf9@gmail.com>
2023-04-16 18:40:02 +0300

    Add some text to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
+        System.out.println("A Change");
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World..." + i);
         }
